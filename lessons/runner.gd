c56ac9a35ababec896e4ca20be43e918f2a36227
thebigjeffy9 <sawong@s.sfusd.edu>
2025-12-12 09:56:05 -0800

    module 9 lesson 8 done

--- lessons/runner.gd
+++ lessons/runner.gd
@@ -1,3 +1,4 @@
+class_name Runner
 extends CharacterBody2D
 
 
@@ -9,6 +10,9 @@ extends CharacterBody2D
 
 @onready var _runner_visual: RunnerVisual = %RunnerVisualRed
 @onready var _dust: GPUParticles2D = %Dust
+
+
+signal walked_to
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -34,3 +38,26 @@ func _physics_process(delta: float) -> void:
 	else:
 		_runner_visual.animation_name = RunnerVisual.Animations.IDLE
 		_dust.emitting = false
+
+
+func walk_to(destination_global_position: Vector2) -> void:
+	
+	var direction := global_position.direction_to(destination_global_position)
+	_runner_visual.angle = direction.orthogonal().angle()
+
+	
+	_runner_visual.animation_name = RunnerVisual.Animations.WALK
+	_dust.emitting = true
+
+	
+	var distance := global_position.distance_to(destination_global_position)
+	var duration :=  distance / (max_speed * 0.2)
+
+	
+	var tween := create_tween()
+	tween.tween_property(self, "global_position", destination_global_position, duration)
+	tween.finished.connect(func():
+		_runner_visual.animation_name = RunnerVisual.Animations.IDLE
+		_dust.emitting = false
+		walked_to.emit()
+	)
