7b18900dab015862e29b3d23152137a6e77e3976
thebigjeffy9 <sawong@s.sfusd.edu>
2025-12-12 14:46:35 -0800

    Module 9 lesson 9 done

--- lessons/game.gd
+++ lessons/game.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @onready var _finish_line: FinishLine = %FinishLine
+@onready var _count_down: CountDown = %CountDown
+@onready var _runner: Runner = %Runner
 
 func _ready() -> void:
 	_finish_line.body_entered.connect(func (body: Node) -> void:
@@ -22,4 +24,10 @@ func _ready() -> void:
 
 	_finish_line.confettis_finished.connect(
 		get_tree().reload_current_scene.call_deferred
+	)
+	_count_down.start_counting()
+	_runner.set_physics_process(false)
+	_count_down.counting_finished.connect(
+		func() -> void:
+			_runner.set_physics_process(true)
 	)
